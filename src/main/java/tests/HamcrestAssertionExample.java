5aec5fa6c184be6d143e547752371824b8b0a522
unknown <christian.bugan@gmail.com>
2023-11-15 13:25:14 +0200

    curs45

--- src/main/java/tests/HamcrestAssertionExample.java
+++ src/main/java/tests/HamcrestAssertionExample.java
@@ -1,23 +1,25 @@
 package tests;
-
+import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotEquals;
 
+import java.util.Arrays;
 import java.util.List;
 
-import org.testng.annotations.Test;
-
-import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
 
+import org.testng.annotations.Test;
 
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
-import static io.restassured.RestAssured.given;
+import static utils.NumberChecker.*;
+import static utils.NumberIsPositive.*;
 
 public class HamcrestAssertionExample {
-
+	
 	
 	@SuppressWarnings("unchecked")
 	@Test
@@ -32,45 +34,49 @@ public class HamcrestAssertionExample {
 		
 		System.out.println(resp.asString());
 		
-		JsonPath jsnPath = resp.jsonPath();
+		JsonPath jsnPath =  resp.jsonPath();
 		String name = jsnPath.getString("name");
 		System.out.println(name);
 		
-		//TestNG assert
+		//TestNg assert
 		assertEquals(name, "Tatooine");
 		
 		//Hamcrest assert
+	//	assertThat(name, "Tatooine");
+	//	assertThat(name, "Tatooine");	
 		assertThat(name, equalTo("Tatooine"));
+		assertThat(name, is("Tatooine")); // is (T value)
+		assertThat(name, is(equalTo("Tatooine")));// is(Matcher<T>)
 		
-		assertThat(name, is("Tatooine")); //face acelasi lucru ca linia de mai sus
-		// is (T Value)
-		
-		assertThat(name, is(equalTo("Tatooine"))); //same thing ca mai sus 
-		// is(Matcher<T>)
-		
-		//TestNG assert
+		//TestNg assert
 		assertNotEquals(name, "Terra");
 		
 		//Hamcrest assert
 		assertThat(name, is(not("Terra")));
 		assertThat(name, is(not(equalTo("Terra"))));
 		assertThat(name, is(not(instanceOf(Integer.class))));
-		
+		assertThat(name, is(instanceOf(String.class)));
+
 		//starts-with
 		assertThat(resp.asString(), startsWith("{\"name\""));
 		assertThat(name, startsWith("Tato"));
 		assertThat(resp.asString(), startsWithIgnoringCase("{\"NAME\""));
-		
+
 		//ends-with
 		assertThat(resp.asString(), endsWith("api/planets/1/\"}"));
-		
-		//contains
+		assertThat(resp.asString(), endsWithIgnoringCase("aPi/pLaNeTs/1/\"}"));
+
+		//containes
 		assertThat(name, containsStringIgnoringCase("tooi"));
 		assertThat(resp.asString(), containsString("desert"));
 		assertThat(name, containsString("tooi"));
 		
 		//pattern
 		assertThat(name, matchesPattern("[A-Za-z]+"));
+		name="Tatooine123";
+		assertThat(name, matchesPattern("[A-Za-z0-9]+"));
+		String diameter =  jsnPath.getString("diameter");
+		assertThat(diameter, matchesPattern("[0-9]+"));
 		
 		List<String> movies = jsnPath.getList("films");
 		System.out.println(movies.get(1));
@@ -82,26 +88,93 @@ public class HamcrestAssertionExample {
 		        "https://swapi.dev/api/films/5/", 
 		        "https://swapi.dev/api/films/6/"));
 		
+
 		assertThat(movies, contains(
 				startsWith("https://sw"), 
 		        endsWith("3/"), 
 		        equalTo("https://swapi.dev/api/films/4/"), 
-		        startsWith("https://swapi.dev/"), 
+		        startsWith("https://swapi.dev"), 
 		        endsWith("api/films/6/")));
 		
+		assertThat(movies, contains(
+				startsWith("https://sw"), 
+		        endsWith("3/"), 
+		        equalTo("https://swapi.dev/api/films/4/"), 
+		        startsWith("https://swapi.dev"), 
+		        equalTo("https://swapi.dev/api/films/6/")));
+	
+		
 		assertThat(movies, hasItem("https://swapi.dev/api/films/6/"));
-		
-		assertThat(movies, hasItems("https://swapi.dev/api/films/6/",  "https://swapi.dev/api/films/4/"));
-		
+		assertThat(movies, hasItems("https://swapi.dev/api/films/6/","https://swapi.dev/api/films/4/"));
 		assertThat(movies, hasItem(startsWith("http")));
 		assertThat(movies, hasItem(containsString("swapi")));
+		assertThat(movies, hasItems(startsWith("http"), containsString("swapi")));
+		
 		
 		assertThat(movies, hasSize(5));
 		assertThat(movies, hasSize(lessThan(10)));
 		assertThat(movies, hasSize(greaterThan(3)));
 		
+		assertThat(movies, both(hasSize(lessThan(6))).and(hasToString(containsString("films/6"))));
+
+		String[] array = {jsnPath.getString("climate"), jsnPath.getString("terrain"),
+				jsnPath.getString("diameter"), jsnPath.getString("gravity"),
+				jsnPath.getString("name")};
+
+		System.out.println(array[0]);
+		assertThat(array, is(not(emptyArray())));
+		assertThat(array, is(not(nullValue())));
+		
+		System.out.println(Arrays.toString(array));
+		
+		assertThat(array, arrayContaining(
+				"arid", "desert", "10465", "1 standard", "Tatooine"
+				));
+		
+		
+		assertThat(array, arrayContainingInAnyOrder(
+				"Tatooine","arid", "desert", "10465", "1 standard" ));
+		
+		System.out.println(resp.asString());
+		
+		assertThat(resp.asString(), containsStringIgnoringCase("ARID") );
+		assertThat(resp.asString(), stringContainsInOrder("name","rotation_period" ));
+
+		//and
+		assertThat(resp.asString(), both(containsString("url")).and(containsString(diameter)));
+		//or
+		assertThat(name, either(is("Tatooine")).or(is("Tatooine2")).or(is(not("Tatooine5"))));
+	
+		/*
+		 *  "rotation_period": "23", 
+    "orbital_period": "304", 
+    "diameter": "10465", 
+    "climate": "arid", 
+    "gravity": "1 standard", 
+    "terrain": "desert", 
+    "surface_water": "1", 
+		 */
+		
+		String rotation = jsnPath.getString("rotation_period");
+		String climate = jsnPath.getString("climate");
+		String gravity = jsnPath.getString("gravity");
+		
+		System.out.println("------------------------------------");
+		System.out.println(rotation);
+		System.out.println(climate);
+		System.out.println(gravity);
+	
+		
+		assertThat(rotation, is(numbersOnly()));
+		assertThat(gravity, is(not(numbersOnly())));
+		
+		assertThat(Integer.parseInt(rotation), is(postiveNumber()));
+		
+		assertThat(-7, is(not(postiveNumber())));
+
+	//	assertThat(-7, is(postiveNumber()));
 		
 	}
 	
-	
+
 }
